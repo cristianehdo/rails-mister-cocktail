d62c61a600388508bea9c19c103cd650c9aa31da
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-09 17:33:20 +0000

    added cards on cocktails  index

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109114612) do
+ActiveRecord::Schema.define(version: 20171109170342) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "doses", force: :cascade do |t|
