1df0dbb41084f7ebe84d22d77d9a817d03d26671
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-09 16:35:16 +0000

    created doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'cocktails#index'
-resources :cocktails, only: [:index, :new, :create, :show]
+resources :cocktails, only: [:index, :new, :create, :show] do
+  resources :doses, only: [:new, :create, :destroy]
+  end
 end
 
 # GET "cocktails"
 # GET "cocktails/42"
 # GET "cocktails/new"
 # POST "cocktails"
+# GET "cocktails/42/doses/new"
+# POST "cocktails/42/doses"
+# DELETE "doses/25"
